18f02547c8c90423729e706d3746b475a94a2fc2
gkgoat <gkgoat6700@gmail.com>
2019-10-16 16:49:28 -0400

    init

---
+++ content.js
@@ -0,0 +1,30 @@
+var canvases = {};
+function cact(canvas,request,respond){
+if(request.type == 'set'){respond(canvas[request.prop] = request.value); return false};
+if(request.type == 'get'){respond(canvas[request.prop]); return false};
+if(request.type in canvas){respond(canvas[request.type](...request.args)); return false}; 
+
+};
+function canvas(request,respond){
+    if(request.type == 'adopt'){canvases[request.id] = $(request.sel).get(0).getContext('2d'); respond({sucess: true}); return false};
+if(!request.id in canvases){respond({error: true}); return false};
+
+return cact(canvases[request.id],request,respond)
+};
+chrome.runtime.onMessage.addListener((request,_,respond) => {
+if(request.type == 'dialog' && request.id.beginsWith(window.location.href)){
+    var index = request.id.indexOf('#');
+    var id = request.id.substring(index + 1);
+    $('#' + id).dialog(request.data).on('dialogopen',(ev,ui) => {respond(ev)})
+return true;
+};
+if(request.type == 'onload' && request.data == window.location.href){
+$((v) => respond({}))
+    return true;
+};
+if(request.type == 'canvas')return canvas(request.data,respond)
+});
+$(function(){
+
+
+}) 
